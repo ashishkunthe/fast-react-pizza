d8547f14589d5f22f89be944fe8d9ed24b373a6a
ashishkunthe <ashishkunthe2003@gmail.com>
2024-10-29 20:36:37 +0530

    tailwind stylings

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -42,30 +42,37 @@ function CreateOrder() {
   const formErrors = useActionData();
 
   return (
-    <div>
-      <h2>Ready to order ? lets go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="text-xl font-semibold mb-8">Ready to order ? lets go!</h2>
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required className="input" />
-        </div>
-
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input className="input" type="tel" name="phone" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <div className="grow">
+            <input type="text" name="customer" required className="input" />
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input" type="tel" name="phone" required />
+
+            {formErrors?.phone && (
+              <p className="mt-2 text-xs text-red-700 bg-red-100 p-2 rounded-md">
+                {formErrors.phone}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
             <input type="text" name="address" required className="input" />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             type="checkbox"
             name="priority"
@@ -75,11 +82,13 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to yo give your order priority?
+          </label>
         </div>
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
         <div>
-          <Button disabled={isSubmitting}>
+          <Button type="primary" disabled={isSubmitting}>
             {isSubmitting ? "Placing the order" : "Order now"}
           </Button>
         </div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,6 +7,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + "px-4 py-3 sm:px-6 sm:py-4",
     small: base + "px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "bg-transparent border-2 border-stone-300 px-4 py-2 md:px-5 md:py-3.5  uppercase font-semibold text-stone-400 hover:bg-stone-300 hover:text-stone-800 inline-block tracking-wide rounded-full  transition-colors duration-300 focus:outline-none  focus:ring focus:ring-offset-2 focus:ring-stone-300  disabled:cursor-not-allowed  ",
   };
   if (to)
     return (
